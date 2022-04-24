9c3c951d6cc6e4dbe5f65005ad4dd5f1fb7c36b3
salochinYom <nrmoy2@gmail.com>
2022-04-24 14:21:42 -0400

    renamed things

--- src/ScreenInterface.h
+++ src/ScreenInterface.h
@@ -8,14 +8,14 @@ class ScreenInterface{
         void init(); //starts the thing
         void updateTopLine(String Text); //updates top line
         void updateBottomLine(String Text); //updates bottom line of text
-        void ClearAll(); //clears screen
-        void ClearTopLine(); //Clears Top Line
-        void ClearBottomLine(); //Clears Bottom Lin
-        void Blink();
+        void clearAll(); //clears screen
+        void clearTopLine(); //Clears Top Line
+        void clearBottomLine(); //Clears Bottom Lin
+        //void Blink();
         
 
 
     private:
-        String TopLine; //stores top 16 chars of text
+        String topLine; //stores top 16 chars of text
         String bottomLine; //stores bottom 16 lines of text
 };
